c090bc984e9740215ebd1f03b3741006e3829c44
glickdv <dvgbiogeek@gmail.com>
2014-08-18 23:36:51 -0700

    140818
    item validation fts

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -9,16 +9,29 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
 
         # Cannot submit an empty list item
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # Error message says list items cannot be blank
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # New item now works
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy cilantro\n')
+        self.check_for_row_in_list_table('1: Buy cilantro')
 
         # Attempts are made for a second blank list submission
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # Again a similiar warning shows up
+        self.check_for_row_in_list_table('1: Buy cilantro')
+        error = self.browser.find_element_by_css_selector('has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # Text saves the day!
+        self.browser.find_element_by_id('id_new_item').send_keys('Make coffee\n')
+        self.check_for_row_in_list_table('1: Buy cilantro')
+        self.check_for_row_in_list_table('2: Make coffee')
 
         self.fail('write me!')
 
