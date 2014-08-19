81dd4b5dda52a7aa88682edaab8c9ad21c66741b
glickdv <dvgbiogeek@gmail.com>
2014-08-18 17:13:09 -0700

    140818
    Static files, bootstrap integration, changes to settings

--- superlists/functional_tests/test.py
+++ superlists/functional_tests/test.py
@@ -1,10 +1,11 @@
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerCase):
 
     # Startup the browser
     def setUp(self):
--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -83,3 +83,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
