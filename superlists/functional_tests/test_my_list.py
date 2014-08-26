27ec27517a8e048ca1b8b86387e0e827da884ea1
glickdv <dvgbiogeek@gmail.com>
2014-08-26 14:34:34 -0700

    user lists - outside in
    fails in functional test - added template, url, view for my_list, a
    link (not yet functional)

--- superlists/functional_tests/test_my_list.py
+++ superlists/functional_tests/test_my_list.py
@@ -32,3 +32,35 @@ class MyListsTest(FunctionalTest):
 
         self.browser.get(self.server_url)
         self.wait_to_be_logged_in(email)
+
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        # Log in user
+        self.create_pre_authenticated_session('edith@example.com')
+
+        # go to home page and start a list
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Reticulate splines\n')
+        self.get_item_input_box().send_keys('Immanentize eschaton\n')
+        first_list_url = self.browser.current_url
+
+        # There is now a "My Lists" link
+        self.browser.find_element_by_link_text('My lists').click()
+
+        # Your list is in there, named according to the first item
+        self.browser,find_element_by_link_text('Reticulate splines').click()
+        self.assertEqual(self.browser.current_url, first_list_url)
+
+        # You decide to start a new list
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Click cows\n')
+        second_list_url = self.browser.current_url
+
+        # Under 'my lists', the new list appears
+        self.browser.find_element_by_link_text('My lists').click()
+        self.browser.find_element_by_link_text('Click cows').click()
+        self.assertEqual(self.browser.current_url, second_list_url)
+
+        # You logout. The 'my lists' option disappears
+        self.browser.find_element_by_id('id_logout').click()
+        self.assertEqual(self.browser.find_element_by_link_text('My lists'), [])
+
