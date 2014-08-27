1271076683a2f5d8415b54c4a932e5f8cf52a3f3
glickdv <dvgbiogeek@gmail.com>
2014-08-27 14:08:43 -0700

    model adjustment (add owner), model testing

--- superlists/lists/tests/test_models.py
+++ superlists/lists/tests/test_models.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
 
 from lists.models import Item, List
+
+User = get_user_model()
 
 
 class ItemModelTest(TestCase):
@@ -59,3 +62,21 @@ class ListModelTest(TestCase):
             [item1, item2, item3]
         )
 
+    def test_create_new_creates_list_and_first_item(self):
+        List.create_new(first_item_text='new item text')
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'new item text')
+        new_list = List.objects.first()
+        self.assertEqual(new_item.list, new_list)
+
+    def test_create_new_optionally_saves_owner(self):
+        user = User.objects.create()
+        List.create_new(first_item_text = 'new item text', owner=user)
+        new_list = List.objects.first()
+        self.assertEqual(new_list.owner, user)
+
+    def test_lists_can_have_owners(self):
+        List(owner=User()) # should not raise
+
+    def test_list_owner_is_optional(self):
+        List().full_clean() # should not raise
