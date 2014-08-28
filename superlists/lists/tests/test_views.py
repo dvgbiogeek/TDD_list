ef18a247ee24abc92b26e1feaf3b418ff7fcc91d
glickdv <dvgbiogeek@gmail.com>
2014-08-28 15:48:44 -0700

    new tests, url, view - one failed unit test
    does not pass one unit test - need to add attribute to list model

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -247,3 +247,25 @@ class NewListViewUnitTest(unittest.TestCase):
             new_list(self.request)
             self.assertFalse(mock_form.save.called)
 
+
+class ShareListTest(TestCase):
+
+    def test_sharing_a_list_via_post(self):
+        sharee = User.objects.create(email='share.with@me.com')
+        list_ = List.objects.create()
+        response = self.client.post(
+            '/lists/%d/share' % (list_.id),
+            {'email': 'share.with@me.com'} 
+        )
+        self.assertIn(sharee, list_.shared_with.all())
+
+    def test_post_redirects_to_lists_page(self):
+        sharee = User.objects.create(email='share.with@me.com')
+        list_ = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/share' % (list_.id),
+            {'email': 'share.with@me.com'}
+        )
+        self.assertRedirects(response, list_.get_absolute_url())
+
